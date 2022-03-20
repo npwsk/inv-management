33e689fcf3602ee1bd8e7144e703441f820d90b4
Nika Perfilova <ees2009.2014@gmail.com>
2022-03-20 14:10:30 +0700

    feat: add navbar to header

--- src/client/components/Header.jsx
+++ src/client/components/Header.jsx
@@ -1,14 +1,36 @@
 import { Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
 const Header = () => {
   return (
     <header>
-      <nav>
-        <Link to="/">Главная</Link>
-        <Link to="/reports">Сформировать отчет</Link>
-        <Link to="/about">Справка</Link>
-        <Link to="/manual">Руководство</Link>
-      </nav>
+      <Navbar collapseOnSelect expand="lg" bg="light" fixed="top">
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>Главная</Navbar.Brand>
+          </LinkContainer>
+
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <LinkContainer to="/reports">
+                <Nav.Link>Сформировать отчет</Nav.Link>
+              </LinkContainer>
+            </Nav>
+
+            <Nav>
+              <LinkContainer to="/about">
+                <Nav.Link>Справка</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/manual">
+                <Nav.Link>Руководство</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
   );
 };
