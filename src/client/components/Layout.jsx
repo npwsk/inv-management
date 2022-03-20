0ac8bf2b2b5f67cb7a3b511e2ef4b47a12463853
Nika Perfilova <ees2009.2014@gmail.com>
2022-03-20 14:37:34 +0700

    fix: fix layout

--- src/client/components/Layout.jsx
+++ src/client/components/Layout.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from 'react-router';
+import { Container } from 'react-bootstrap';
 
 import Header from './Header';
 
 const Layout = () => {
   return (
-    <div>
+    <div style={{ paddingTop: '56px' }}>
       <Header />
-      <Outlet />
+      <Container>
+        <Outlet />
+      </Container>
     </div>
   );
 };
