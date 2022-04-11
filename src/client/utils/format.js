bab823f253d99bf21dfa6b197ac18755fd967e63
Nika Perfilova <ees2009.2014@gmail.com>
2022-04-11 20:37:53 +0700

    fix: fix format functions

--- src/client/utils/format.js
+++ src/client/utils/format.js
@@ -1,9 +1,7 @@
-export const staffToStr = (obj = {}) =>
-  Object.entries(obj)
-    .filter(([prop]) => prop !== 'id')
-    .map(([, val]) => val)
-    .join(' ');
+export const staffToStr = (staff = {}) =>
+  `${staff.last_name} ${staff.first_name} ${staff.middle_name}`;
 
-export const locationToStr = (location = {}) => `${location.building}, ${location.room} каб.`;
+export const locationToStr = (location = {}) =>
+  `Корпус ${location.building}, ${location.room} ауд.`;
 
 export const formatDate = (str) => new Date(str).toLocaleDateString('ru-RU');
